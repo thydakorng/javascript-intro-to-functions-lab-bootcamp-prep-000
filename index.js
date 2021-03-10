9152ca7e176478bae308c127822e06d519de1d0d
Thyda Korng <thydakorng@gmail.com>
2021-03-10 19:21:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
 function whisper(string) {
   return string.toLowerCase()
 }
+
 function logShout(string) {
-  console.log(toUpperCase)
+  console.log(logShout.toUpperCase)
 }
