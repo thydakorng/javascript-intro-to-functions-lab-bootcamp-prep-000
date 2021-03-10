b6d341a092a2d33748fdb26b287f64707bcc1faa
Thyda Korng <thydakorng@gmail.com>
2021-03-10 19:38:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,5 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  return string.toLowerCase()
     expect(sayHiToGrandma('hello'.toLowerCase)).toEqual("I can't hear you!")
   }
