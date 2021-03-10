73d0afb7bc142141f6f68b6d5ba41f4ec214c448
Thyda Korng <thydakorng@gmail.com>
2021-03-10 19:42:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,14 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
+    expect(sayHiToGrandma('hello'.toLowerCase()).toEqual("I can't hear you!")
 
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma('HELLO'.toUpperCase()).toEqual("YES INDEED!")
+ 
     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
   }
